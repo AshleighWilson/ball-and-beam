9d1e0e96b2fbf7ff931f83f49720e7ab986c7f7f
Ashleigh Wilson <ashleigh.wilson85@icloud.com>
2021-08-15 17:18:14 +0100

    Basic magnetic sensor test added.

--- ball-and-beam.ino
+++ ball-and-beam.ino
@@ -1,5 +1,25 @@
+#include <SimpleFOC.h>
+
+// Motor sensor configuration.
+#define MS_PIN A5
+#define MS_RAW_MIN 4
+#define MS_RAW_MAX 926
+
+// Create our magnetic sensor object.
+MagneticSensorPWM motorSensor = MagneticSensorPWM(MS_PIN, MS_RAW_MIN, MS_RAW_MAX);
+
 void setup() {
+  Serial.begin(115200);
+
+  // Initialise the magnetic sensor.
+  motorSensor.init();
+  _delay(1000);
 }
 
 void loop() {
+  // Print the sensor angle and velocity every 100ms.
+  Serial.print(motorSensor.getAngle());
+  Serial.print("\t");
+  Serial.println(motorSensor.getVelocity());
+  _delay(100);
 }
